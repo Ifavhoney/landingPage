a5b4b7314ecd2f06cf0860c83a9c8338b10f9699
Ifavhoney <nuthsaid@outlook.com>
2019-04-26 13:03:54 -0400

    Completed Feature, and service with wow - animate css

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -8,7 +8,10 @@ Date: February 08 2019
 //IIFE
 (function () {
 
+  //Events
+
   //Btn to set the action 
+
   $("#btn-1").click(() => {
     animate("#anim-head", "bounce")
     animate("#anim-body", "slideInLeft")
@@ -22,20 +25,45 @@ Date: February 08 2019
 
   })
 
+  $("#rex-btn").click(() => {
+    animate("#rex-btn", "bounceInDown")
+  })
+
+  //Run time
+  animate("#rex-btn", "slideInRight")
+
+  animate(".rex-feature", "slideInRight")
+
+  animate(".rex-feature-economy", "lightSpeedIn")
+
+  //Need answer  on why it doesnt work
+  animate(".rex-feature-responsive", "bounceIn");
+
+  animate(".rex-feature-private", "rollIn");
+
+  animate(".rex-num", "fadeIn")
+
+  animate(".rex-content", "fadeInUp")
+
+
+
   //Animates Elements
   function animate(element, animation) {
 
     let animation_txt = "animated " + animation;
-
+    console.log(animation_txt)
     $(element).addClass(animation_txt);
     setTimeout(() => {
 
+      //runs only once 
       $(element).removeClass(animation_txt)
 
     }, 1000)
 
 
   }
+
+
 
   function Start() {
     console.log(
