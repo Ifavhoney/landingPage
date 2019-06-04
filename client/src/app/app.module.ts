c06357a906ce9baad23c98e1316fde3e3ba5f6e9
orr1nuycruz <orr1n@hotmail.com>
2019-06-04 16:50:17 -0400

    angular updated. Animation not working still...

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
+import { BodyComponent } from './partials/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
